6fb25b9ccf64f2fe87b9b85f915bd7c022404d18
James Nylen <jnylen@gmail.com>
2019-01-31 02:23:50 -0500

    Add new tests

---
+++ test/basic-line-6.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env go-scad
+
+end_cap_sides(6);
+pendown();
+forward(10);
+penup();
---
+++ test/basic-line2-6.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env go-scad
+
+end_cap_sides(6);
+pendown();
+forward(5);
+forward(5);
+penup();
