cbe1e1247712033e2e7b12aefd725f203f10f664
James Nylen <jnylen@gmail.com>
2020-02-24 07:51:11 +0000

    Fix the way headings (angles between points) are stored and processed
    A heading is a property shared by two neighboring points, so it doesn't
    make sense to store this on a point object itself.  Store data as a
    TurtlePolygon instead, that knows its own Points and Headings.
    
    The Headings array is redundant with the point coordinates but this is
    stored to avoid a lot of recalculation of the angles between points.

--- main.go
+++ main.go
@@ -68,14 +68,13 @@ func degSin(deg float64) float64 {
 type TurtlePoint struct {
 	X           float64
 	Y           float64
-	Heading     float64
 	Thickness   float64
 	EndCapSides int
 }
 
-type Point struct {
-	X float64
-	Y float64
+type TurtlePolygon struct {
+	Points   []TurtlePoint
+	Headings []float64
 }
 
 var stripZeroes *regexp.Regexp
@@ -142,35 +141,39 @@ func jsToScad(jsInput string) string {
 
 	// Internal state variables
 	turtlePendown := false
-	turtlePolygons := make([][]TurtlePoint, 0)
-	turtlePoints := make([]TurtlePoint, 0)
+	turtlePolygons := make([]TurtlePolygon, 0)
 	var turtlePenSize float64 = 1
 	var turtleEndCapSides int = 60
 	var turtleX float64 = 0
 	var turtleY float64 = 0
 	var turtleHeading float64 = 0
+	var turtlePolygon TurtlePolygon
 
 	// Set up functions
 	vm.Set("pendown", func(call otto.FunctionCall) otto.Value {
 		if !turtlePendown {
 			turtlePendown = true
-			turtlePoints = []TurtlePoint{{
-				X:           turtleX,
-				Y:           turtleY,
-				Heading:     turtleHeading,
-				Thickness:   turtlePenSize,
-				EndCapSides: turtleEndCapSides,
-			}}
+			turtlePolygon = TurtlePolygon{
+				Points: []TurtlePoint{{
+					X:           turtleX,
+					Y:           turtleY,
+					Thickness:   turtlePenSize,
+					EndCapSides: turtleEndCapSides,
+				}},
+				Headings: make([]float64, 0),
+			}
 		}
 		return otto.UndefinedValue()
 	})
 	vm.Set("penup", func(call otto.FunctionCall) otto.Value {
 		if turtlePendown {
 			turtlePendown = false
-			// TODO is this necessary?
-			polygon := make([]TurtlePoint, len(turtlePoints))
-			copy(polygon, turtlePoints)
-			turtlePolygons = append(turtlePolygons, turtlePoints)
+			if len(turtlePolygon.Points) != len(turtlePolygon.Headings)+1 {
+				log.Fatalf("Bad polygon: points=%d headings=%d",
+					len(turtlePolygon.Points),
+					len(turtlePolygon.Headings))
+			}
+			turtlePolygons = append(turtlePolygons, turtlePolygon)
 		}
 		return otto.UndefinedValue()
 	})
@@ -196,13 +199,13 @@ func jsToScad(jsInput string) string {
 		turtleX += d * degCos(turtleHeading)
 		turtleY += d * degSin(turtleHeading)
 		if turtlePendown {
-			turtlePoints = append(turtlePoints, TurtlePoint{
+			turtlePolygon.Points = append(turtlePolygon.Points, TurtlePoint{
 				X:           turtleX,
 				Y:           turtleY,
-				Heading:     turtleHeading,
 				Thickness:   turtlePenSize,
 				EndCapSides: turtleEndCapSides,
 			})
+			turtlePolygon.Headings = append(turtlePolygon.Headings, turtleHeading)
 		}
 		return otto.UndefinedValue()
 	})
@@ -236,9 +239,9 @@ func jsToScad(jsInput string) string {
 	for _, polygon := range turtlePolygons {
 		outBeginPolygon()
 
-		if len(polygon) == 1 {
+		if len(polygon.Points) == 1 {
 			// Degenerate case: just draw an end cap
-			point := polygon[0]
+			point := polygon.Points[0]
 			for j := 0; j < point.EndCapSides; j++ {
 				angle := float64(j) * 360 / float64(point.EndCapSides)
 				outPoint(
@@ -253,34 +256,32 @@ func jsToScad(jsInput string) string {
 		d := 1
 		i := 0
 		for {
-			point := polygon[i]
-			var headingPrev float64 = math.NaN()
-			if i > 0 {
-				headingPrev = polygon[i-1].Heading
-			}
+			point := polygon.Points[i]
 			if i == 0 {
 				// Draw begin cap
+				headingBegin := polygon.Headings[0]
 				for j := 0; j <= point.EndCapSides/2; j++ {
-					angle := point.Heading - 90 - float64(j)*360/float64(point.EndCapSides)
+					angle := headingBegin - 90 - float64(j)*360/float64(point.EndCapSides)
 					outPoint(
 						point.X+point.Thickness/2*degCos(angle),
 						point.Y+point.Thickness/2*degSin(angle),
 						j == point.EndCapSides/2)
 				}
 				outNewLine()
-			} else if i == len(polygon)-1 {
+			} else if i == len(polygon.Points)-1 {
 				// Draw end cap
-				if len(polygon) > 2 {
+				if len(polygon.Points) > 2 {
 					outNewLine()
 				}
+				headingEnd := polygon.Headings[i-1]
 				for j := 0; j <= point.EndCapSides/2; j++ {
-					angle := headingPrev + 90 - float64(j)*360/float64(point.EndCapSides)
+					angle := headingEnd + 90 - float64(j)*360/float64(point.EndCapSides)
 					outPoint(
 						point.X+point.Thickness/2*degCos(angle),
 						point.Y+point.Thickness/2*degSin(angle),
 						j == point.EndCapSides/2)
 				}
-				if len(polygon) > 2 {
+				if len(polygon.Points) > 2 {
 					outNewLine()
 				}
 			} else {
@@ -293,7 +294,7 @@ func jsToScad(jsInput string) string {
 					((i == len(polygon)-2 && d == 1) || (i == 1 && d == -1)))
 			}
 
-			if i == len(polygon)-1 && d == 1 {
+			if i == len(polygon.Points)-1 && d == 1 {
 				d = -1
 			}
 			if i == 1 && d == -1 {
